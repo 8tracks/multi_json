e6ef0313d9fe2e717b253f153cbac084c9e119d7
Ivayr Farah Netto <nettofarah@gmail.com>
2014-12-09 14:52:01 -0800

    do not raise for now

--- lib/multi_json.rb
+++ lib/multi_json.rb
@@ -118,8 +118,8 @@ module MultiJson
     begin
       adapter.load(string, options)
     rescue adapter::ParseError => exception
-      MLOG.log('fb_response_error', { string: string, error: exception })
-      raise ParseError.build(exception, string)
+      MLOG.log('fb_response_error', { string: string })
+      #raise ParseError.build(exception, string)
     end
   end
   alias decode load
